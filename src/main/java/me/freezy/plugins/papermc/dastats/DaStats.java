98cd0645ccb6a21dc6ffe5c1fde52c2e09be1b6b
DaTTV <admin@freezy.me>
2025-03-07 11:05:59 +0100

    init

---
+++ src/main/java/me/freezy/plugins/papermc/dastats/DaStats.java
@@ -0,0 +1,17 @@
+package me.freezy.plugins.papermc.dastats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DaStats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
